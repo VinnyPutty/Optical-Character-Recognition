050e74b5ceece0b472481404a0f76bce73e39710
VinnyPutty <vinayputreddi@gmail.com>
2015-05-20 14:06:31 -0700

    Tile Saving
    Tile Saving Method Added

--- CharExtract.java
+++ CharExtract.java
@@ -1,6 +1,12 @@
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
 
 public class CharExtract {
 
@@ -31,7 +37,7 @@ public class CharExtract {
 		for (int row = 0; row < pixels.length; row++) {
 			for (int col = 0; col < pixels[row].length; col++) {
 				if (pixels[row][col] > t && !withinTiles(row, col)) {
-					tiles.add( new Tile( new Mask(col, row, t, pixels) ) );
+					tiles.add(new Tile(new Mask(col, row, t, pixels)));
 				}
 			}
 		}
@@ -76,35 +82,28 @@ public class CharExtract {
 			}
 		}
 		int[] weight = new int[256];
-		for( int i = 0; i < 256; i++ )
-		{
+		for (int i = 0; i < 256; i++) {
 			weight[i] = bin[i] * (i + 1);
 		}
 
 		// balanced histogram thresholding
 		// average intensity
-		int aMean = Math.round( partialArraySum(weight, 0, 255)/
-				partialArraySum(bin, 0, 255) );
+		int aMean = Math.round(partialArraySum(weight, 0, 255) / partialArraySum(bin, 0, 255));
 		// find means below and above the average intensity
-		double lMean = partialArraySum(weight, 0, aMean)/
-				partialArraySum(bin, 0, 255);
-		double hMean = partialArraySum(weight, aMean, 0)/
-				partialArraySum(bin, aMean, 0);
+		double lMean = partialArraySum(weight, 0, aMean) / partialArraySum(bin, 0, 255);
+		double hMean = partialArraySum(weight, aMean, 0) / partialArraySum(bin, aMean, 0);
 		int temp = aMean;
-		aMean = (int) Math.round( ( lMean + hMean ) / 2 );
+		aMean = (int) Math.round((lMean + hMean) / 2);
 
 		// iterate until upper and lower averages are the same
-		while( Math.abs( temp - aMean ) >= 1 )
-		{
+		while (Math.abs(temp - aMean) >= 1) {
 			temp = aMean;
-			lMean = partialArraySum(weight, 0, aMean)/
-					partialArraySum(bin, 0, 255);
-			hMean = partialArraySum(weight, aMean, 0)/ 
-					partialArraySum(bin, aMean, 0);
-			aMean = (int) Math.round( ( lMean + hMean ) / 2 );
+			lMean = partialArraySum(weight, 0, aMean) / partialArraySum(bin, 0, 255);
+			hMean = partialArraySum(weight, aMean, 0) / partialArraySum(bin, aMean, 0);
+			aMean = (int) Math.round((lMean + hMean) / 2);
 		}
 		// calculate t from balanced intensity
-		t = ( aMean - 1.0 ) / ( 256 );
+		t = (aMean - 1.0) / (256);
 		return t;
 	}
 
@@ -114,32 +113,52 @@ public class CharExtract {
 	 * @param a
 	 *            the array
 	 * @param s
-	 * 			  the start index
+	 *            the start index
 	 * @param e
-	 * 			  the end index
+	 *            the end index
 	 * @return the partial array sum
 	 */
-	private int partialArraySum( int[] a , int s , int e )
-	{
+	private int partialArraySum(int[] a, int s, int e) {
 		int sum = 0;
-		for( int i = s; i <= e; i++ )
-		{
+		for (int i = s; i <= e; i++) {
 			sum += a[i];
 		}
 		return sum;
 	}
 
 	// create strings from tiles
-	public String charBuilder()
-	{
+	public String charBuilder() {
 		StringBuilder s = new StringBuilder();
 
 		return s.toString();
 	}
 
-	private char charExtract( Tile t )
-	{
+	private char charExtract(Tile t) {
 
+		return ' ';
+	}
+
+	private void tileSave() {
+		for (int i = 0; i < tiles.size(); i++) {
+			BufferedImage image = new BufferedImage((int) tiles.get(i).getHeight(), (int) tiles.get(i).getWidth(), BufferedImage.TYPE_BYTE_GRAY);
+
+			Iterator<Point> it = tiles.get(i).getMask().getPoints().iterator();
+
+			Point p = it.next();
+			image.setRGB(p.x, p.y, Color.BLACK.getRGB());
+			while (it.hasNext()) {
+				p = it.next();
+				image.setRGB(p.x, p.y, Color.BLACK.getRGB());
+			}
+
+			try {
+				ImageIO.write(image, "png", new File(i + ".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
 
 	}
 
--- Tile.java
+++ Tile.java
@@ -3,11 +3,18 @@ import java.awt.Rectangle;
 public class Tile extends Rectangle {
 
 	private Mask mask;
-	
-	public Tile( Mask m )
-	{
-		super( m.getTile() );
+
+	public Tile(Mask m) {
+		super(m.getTile());
 		mask = m;
+	}
+
+	public Mask getMask() {
+		return mask;
+	}
+
+	public void setMask(Mask mask) {
+		this.mask = mask;
 	}
 
 }
